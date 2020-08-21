85f504cc1b2d11e5c06af937aaa21eb81249fc4a
ScottHenshaw86 <69444386+ScottHenshaw86@users.noreply.github.com>
2020-08-21 22:59:12 +0900

    portfolio page improved for mobile
    added swipe to change slide functionality

--- js/portfolio.js
+++ js/portfolio.js
@@ -90,3 +90,61 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+function detectSwipe(f) {
+    var detect = {
+        startX: 0,
+        startY: 0,
+        endX: 0,
+        endY: 0,
+        minX: 60,   // min X swipe for horizontal swipe
+        maxX: 30,   // max X difference for vertical swipe
+        minY: 50,   // min Y swipe for vertial swipe
+        maxY: 60    // max Y difference for horizontal swipe
+    },
+        direction = null,
+        element = document.body;
+
+    element.addEventListener('touchstart', function (event) {
+        var touch = event.touches[0];
+        detect.startX = touch.screenX;
+        detect.startY = touch.screenY;
+    });
+
+    element.addEventListener('touchmove', function (event) {
+        // event.preventDefault();
+        var touch = event.touches[0];
+        detect.endX = touch.screenX;
+        detect.endY = touch.screenY;
+    });
+
+    element.addEventListener('touchend', function (event) {
+        if (
+            // Horizontal move.
+            (Math.abs(detect.endX - detect.startX) > detect.minX)
+                && (Math.abs(detect.endY - detect.startY) < detect.maxY)
+        ) {
+            direction = (detect.endX > detect.startX) ? 'right' : 'left';
+        } else if (
+            // Vertical move.
+            (Math.abs(detect.endY - detect.startY) > detect.minY)
+                && (Math.abs(detect.endX - detect.startX) < detect.maxX)
+        ) {
+            direction = (detect.endY > detect.startY) ? 'down' : 'up';
+        }
+
+        if ((direction !== null) && (typeof f === 'function')) {
+            f(direction);
+        }
+    });
+}
+
+detectSwipe(myfunction);
+
+function myfunction(direction) {
+    if (direction === 'right') {
+        plusSlides(1)
+    } else if (direction === 'left') {
+        plusSlides(-1)
+    }
+}
